e7fec90d14bb46d874f38b90016b42b0065d4eeb
Chase Gorectke <nbvikingsidiot001@gmail.com>
2014-05-27 18:44:42 -0400

    Better library set up that can be run within the simulator and be used to git pull and update much easier.

--- CGFlowControllerTests/CGFlowControllerTests.m
+++ CGFlowControllerTests/CGFlowControllerTests.m
@@ -2,7 +2,7 @@
 //  CGFlowControllerTests.m
 //  CGFlowControllerTests
 //
-//  Created by Chase Gorectke on 3/28/14.
+//  Created by Chase Gorectke on 5/27/14.
 //  Copyright (c) 2014 Revision Works, LLC. All rights reserved.
 //
 
